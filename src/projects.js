24d8f9ba4cd373195977ef5b4b3d05aac150bb5b
woftis <stevenwalker1991@googlemail.com>
2022-11-16 11:43:54 +0000

    set up projects file and added functions for managing projects

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+import { v4 as uuid4 } from 'uuid';
+
+let projectList = [];
+
+const Project = (title, description, type) => {
+    const id = uuid4();
+    return {id, title, description, type}
+}
+
+
+const newProject = (title, description, type) => {
+    const newProject = Project(title, description, type);
+    projectList.push(newProject);
+}
+
+const editProject = (id, title, description, type) => {
+    const index = projectList.findIndex(project => project.id == id);
+    projectList[index].title = title;
+    projectList[index].description = description;
+    projectList[index].type = type;
+}
+
+const deleteProject = (id) => {
+    const index = projectList.findIndex(project => project.id === id);
+    projectList.splice(index, 1);
+}
+
+export {
+    projectList,
+    newProject,
+    editProject,
+    deleteProject
+
+}
